04ef96b0e235b105d49d97e3e3c3ced61be594ad
Ty Mazey <tymazey@gmail.com>
2019-04-03 13:45:16 -0600

    adds instance method test to search facade

--- spec/facades/search_facade_spec.rb
+++ spec/facades/search_facade_spec.rb
@@ -9,6 +9,23 @@ RSpec.describe "SearchFacade" do
   end
 
   describe 'instance methods' do
+    describe '.total_students' do
+      it 'returns the count of all students in the search' do
+        facade = SearchFacade.new({house: 'Slytherin'})
 
+        expect(facade.total_students).to eq(22)
+      end
+    end
+
+    describe '.students' do
+      it 'returns student objects from the search' do
+        facade = SearchFacade.new({house: 'Slytherin'})
+
+        expect(facade.students.count).to eq(22)
+        expect(facade.students.first).to be_a(Student)
+        expect(facade.students.first.name).to eq('')
+        expect(facade.students.first.id).to eq('')
+      end
+    end
   end
 end
