0b7ce0d8243b3886613e6200167a91f0830d0eac
Ty Mazey <tymazey@gmail.com>
2019-04-03 13:47:41 -0600

    adds methods to facade, hard coded count, build for service and student model next

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -3,4 +3,14 @@ class SearchFacade
   def initialize(search_params)
     @hosue = search_params[:house]
   end
+
+  def total_students
+    22
+  end
+
+  def students
+    HogwartsService(@house).map do |student_info|
+      Student.new(student_info)
+    end
+  end
 end
